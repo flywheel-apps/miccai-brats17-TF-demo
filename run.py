bb8fcfd2a827998f70a899c8c22291ead1f7acbc
Joshua Jacobs, Ph.D <joshicola@gmail.com>
2019-09-21 13:14:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     context = flywheel.GearContext()
     context.custom_dict={}
     # Create a 'dry run' flag for debugging
-    context.custom_dict['dry-run'] = True
+    context.custom_dict['dry-run'] = False
 
     context.log = get_custom_logger('[flywheel:taigw/brats17]')
 
